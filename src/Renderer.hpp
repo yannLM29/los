ca3ec50ba441dac2b7ccf6c579ea329143c55259
yannLM29 <yann.lemasson33@gmail.com>
2025-01-24 18:55:03 +0100

    add renderer and iwindow

---
+++ src/Renderer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "glm/vec3.hpp"
+
+using vec3 = glm::vec3;
+namespace los
+{
+
+class Renderer
+{
+private:
+    /* data */
+public:
+    Renderer(/* args */);
+    ~Renderer();
+    vec3 test();
+};
+    
+    
+} // namespace los
---
+++ src/wrappers/IWindow.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace los
+{
+    class IWindow
+    {
+    private:
+        /* data */
+    public:
+        IWindow(/* args */);
+        ~IWindow();
+    };
+    
+} // namespace los
