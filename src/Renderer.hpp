77ef3e06a664ee95a755fcddb5bc08f12b149895
yannLM29 <yann.lemasson33@gmail.com>
2025-01-26 12:02:27 +0100

    Renderer declaration

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,19 +1,49 @@
 #pragma once
 
-#include "glm/vec3.hpp"
+#include "glm/glm.hpp"
+#include <memory>
 
 using vec3 = glm::vec3;
 namespace los
 {
 
+enum eShaderTypes {
+    kMonochrome = 0,
+    kColored,
+    kTextured
+};
+
+enum eDrawingModes {
+    kFilled = 0,
+    kWireFrame
+};
+
+class IWindow;
+class FileLoader;
+class Camera;
+
 class Renderer
 {
 private:
-    /* data */
+    IWindow &mWindowRef;
+    FileLoader &mFileLoaderRef;
+
+    std::shared_ptr<Camera> mCamera;
+
 public:
-    Renderer(/* args */);
+    Renderer(IWindow &window_ref, FileLoader &fileloader_ref);
     ~Renderer();
-    vec3 test();
+    
+    glm::mat4x4 getProjectionMatrix() const;
+    
+    void clearScreen();
+    void useShader(eShaderTypes type);
+    void setDrawingMode(eDrawingModes mode);
+
+    template<typename T>
+    bool setUniformofCurrentShader(const std::string name, const T &value);
+    
+    
 };
     
     
