33c098e10536659f50cb0415afb8b43e9fef73e6
yannLM29 <yann.lemasson33@gmail.com>
2025-02-02 18:45:47 +0100

    add shaders

---
+++ data/shaders/texture-fragment.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform vec3 lightColor;
+
+void main()
+{
+    vec4 textureColor = texture(texture1, TexCoord);
+    FragColor = vec4(lightColor * textureColor.xyz, textureColor.w);
+}
+
